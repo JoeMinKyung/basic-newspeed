70f85d68d5bc4e1bf8269c8d5fd8af74effe36f5
JoeMinKyung <mk981015@naver.com>
2025-03-12 18:40:40 +0900

    feat: 유저 조회 API

--- domain/user/controller/UserController.java
+++ domain/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.springbasicnewspeed.domain.user.controller;
 
+import com.example.springbasicnewspeed.domain.auth.dto.AuthUser;
+import com.example.springbasicnewspeed.domain.common.annotation.Auth;
 import com.example.springbasicnewspeed.domain.user.dto.UserResponse;
 import com.example.springbasicnewspeed.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +16,9 @@ public class UserController {
 
     private final UserService userService;
 
+    // 로그인한 유저만 전체 유저를 조회할 수 있는 API
     @GetMapping("/users")
-    public List<UserResponse> getUsers() {
-
+    public List<UserResponse> getUsers(@Auth AuthUser authUser) {
+        return userService.findAll();
     }
 }

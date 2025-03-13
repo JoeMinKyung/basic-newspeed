29bd6aec49317a889b856351825bfd97b2241850
JoeMinKyung <mk981015@naver.com>
2025-03-13 23:10:52 +0900

    feat: password 예외처리 추가

--- domain/auth/dto/SignupRequest.java
+++ domain/auth/dto/SignupRequest.java
@@ -1,9 +1,24 @@
 package com.example.springbasicnewspeed.domain.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class SignupRequest {
 
+    @NotBlank @Email(message="이메일 형식을 지켜주세요.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 항목입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,}",
+            message = "비밀번호는 8자 이상이며, 영어, 숫자, 특수문자를 최소 1글자 이상 포함해야 합니다.")
+    private String password;
+
+    @NotBlank(message = "비밀번호 확인은 필수 항목입니다.")
+    public String passwordCheck;
+
+    @NotBlank
+    private String userName;
 }

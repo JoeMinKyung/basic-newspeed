4605eae8c86b706c0eb720288bff7d0d447e63de
JoeMinKyung <mk981015@naver.com>
2025-03-14 15:55:16 +0900

    feat: IllgalStateException -> custom Exception으로 변경 및 불필요한 검증 제거

--- domain/user/service/UserService.java
+++ domain/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.springbasicnewspeed.domain.user.service;
 
+import com.example.springbasicnewspeed.common.exception.*;
 import com.example.springbasicnewspeed.config.PasswordEncoder;
 import com.example.springbasicnewspeed.domain.auth.dto.AuthUser;
 import com.example.springbasicnewspeed.domain.user.dto.request.PasswordUpdateRequest;
@@ -38,19 +39,19 @@ public class UserService {
     @Transactional
     public UserProfileResponse update(AuthUser authUser, UserRequest request) {
         User user = userRepository.findById(authUser.getId()).orElseThrow(
-                () -> new IllegalStateException("존재하지 않는 유저입니다.")
+                () -> new UserNotFoundException("존재하지 않는 유저입니다.")
         );  // 레파짓토리에서 찾는거 말고 다른방법도??
 
         if (request.getEmail() != null && !request.getEmail().isEmpty()) {
             if (userRepository.existsByEmail(request.getEmail())) {
-                throw new IllegalStateException("이미 존재하는 이메일이기 때문에 수정할 수 없습니다.");
+                throw new EmailAlreadyExistsException("이미 존재하는 이메일이기 때문에 수정할 수 없습니다.");
             }
             user.updateEmail(request.getEmail());
         }
 
         if (request.getUserName() != null && !request.getUserName().isEmpty()) {
             if (userRepository.existsByUserName(request.getUserName())) {
-                throw new IllegalStateException("이미 존재하는 닉네임이기 때문에 수정할 수 없습니다.");
+                throw new UserNameAlreadyExistsException("이미 존재하는 닉네임이기 때문에 수정할 수 없습니다.");
             }
             user.updateUserName(request.getUserName());
         }
@@ -62,23 +63,19 @@ public class UserService {
     @Transactional
     public void updatePassword(AuthUser authUser, PasswordUpdateRequest request) {
         User user = userRepository.findById(authUser.getId()).orElseThrow(
-                () -> new IllegalStateException("해당 유저가 존재하지 않습니다.")
+                () -> new UserNotFoundException("해당 유저가 존재하지 않습니다.")
         );
 
         if (!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
-            throw new IllegalStateException("현재 비밀번호가 일치하지 않습니다.");
-        }
-
-        if (request.getNewPassword().length() < 8) {
-            throw new IllegalStateException("새 비밀번호는 8자 이상이어야 합니다.");
+            throw new PasswordMismatchException("현재 비밀번호가 일치하지 않습니다.");
         }
 
         if (!Objects.equals(request.getNewPassword(), request.getNewPasswordCheck())) {
-            throw new IllegalStateException("새 비밀번호와 비밀번호 확인이 같지 않습니다.");
+            throw new PasswordMismatchException("새 비밀번호와 비밀번호 확인이 같지 않습니다.");
         }
 
         if (passwordEncoder.matches(request.getNewPassword(), user.getPassword())) {
-            throw new IllegalStateException("현재 비밀번호와 동일한 비밀번호로 변경할 수 없습니다.");
+            throw new SamePasswordException("현재 비밀번호와 동일한 비밀번호로 변경할 수 없습니다.");
         }
 
         user.updatePassword(passwordEncoder.encode(request.getNewPassword()));

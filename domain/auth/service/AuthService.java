cf2cd44cf78e774751e3314bd0e502e064fba310
JoeMinKyung <mk981015@naver.com>
2025-03-14 15:19:32 +0900

    feat: IllegalStateException -> custom Exception

--- domain/auth/service/AuthService.java
+++ domain/auth/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.example.springbasicnewspeed.domain.auth.service;
 
+import com.example.springbasicnewspeed.common.exception.*;
 import com.example.springbasicnewspeed.config.JwtUtil;
 import com.example.springbasicnewspeed.config.PasswordEncoder;
 import com.example.springbasicnewspeed.domain.auth.dto.SigninRequest;
@@ -24,19 +25,20 @@ public class AuthService {
     @Transactional
     public void signup(SignupRequest request) {
 
-        if (!Objects.equals(request.getPassword(), request.getPasswordCheck())) {
-            throw new IllegalStateException("비밀번호와 비밀번호 확인이 같지 않습니다.");
-        }
-
         if (userRepository.existsByEmail(request.getEmail())) {
-            throw new IllegalStateException("이미 존재하는 이메일입니다.");
+            throw new EmailAlreadyExistsException("이미 존재하는 이메일입니다.");
         }
 
         if (userRepository.existsByUserName(request.getUserName())) {
-            throw new IllegalStateException("이미 존재하는 닉네임입니다.");
+            throw new UserNameAlreadyExistsException("이미 존재하는 닉네임입니다.");
+        }
+
+        if (!Objects.equals(request.getPassword(), request.getPasswordCheck())) {
+            throw new PasswordMismatchException("비밀번호와 비밀번호 확인이 같지 않습니다.");
         }
 
         String encodedPassword = passwordEncoder.encode(request.getPassword());
+
         User user = new User(request.getEmail(), encodedPassword, request.getUserName());
 
         userRepository.save(user);
@@ -45,11 +47,11 @@ public class AuthService {
     @Transactional(readOnly = true)
     public SigninResponse signin(SigninRequest request) {
         User user = userRepository.findByEmail(request.getEmail()).orElseThrow(
-                () -> new IllegalStateException("이메일이 존재하지 않습니다.")
+                () -> new EmailNotFoundException("이메일이 존재하지 않습니다.")
         );
 
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-            throw new IllegalStateException("잘못된 비밀번호입니다.");
+            throw new IncorrectPasswordException("잘못된 비밀번호입니다.");
         }
 
         String bearerJwt = jwtUtil.createToken(user.getId(), user.getEmail());

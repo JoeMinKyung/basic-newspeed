95215e21b79f16207dbcd7b9c8a199a7d29854d7
JoeMinKyung <mk981015@naver.com>
2025-03-14 15:54:01 +0900

    chore: message 수정, 비밀번호 Pattern regexp 수정

--- domain/user/dto/request/PasswordUpdateRequest.java
+++ domain/user/dto/request/PasswordUpdateRequest.java
@@ -9,16 +9,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PasswordUpdateRequest {
 
-    @NotBlank(message = "현재 비밀번호를 입력해야 합니다.")
+    @NotBlank(message = "현재 비밀번호는 필수 항목입니다")
     private String currentPassword;
 
-    @NotBlank(message = "새 비밀번호를 입력해야 합니다.")
+    @NotBlank(message = "새 비밀번호는 필수 항목입니다")
     @Pattern(
-            regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,}",
-            message = "비밀번호는 8자 이상이며, 영어, 숫자, 특수문자를 최소 1글자 이상 포함해야 합니다."
+            regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+=-]).{8,}$",
+            message = "비밀번호는 8자 이상이며, 영어, 숫자, 특수문자를 최소 1개 이상 포함해야 합니다."
     )
     private String newPassword;
 
-    @NotBlank(message = "새 비밀번호 확인을 입력해야 합니다.")
+    @NotBlank(message = "새 비밀번호 확인은 필수 항목입니다")
     private String newPasswordCheck;
 }

6a98b078955d193410cb0d1d57a1310e6f68ea18
JoeMinKyung <mk981015@naver.com>
2025-03-15 03:33:27 +0900

    refactor: response user -> userName으로 변경

--- domain/post/dto/response/PostResponse.java
+++ domain/post/dto/response/PostResponse.java
@@ -9,17 +9,19 @@ import java.time.LocalDateTime;
 public class PostResponse {
 
     private final Long id;
-    private final UserNameResponse userNameResponse;
+    private String userName;
     private final String title;
     private final String content;
+    private final int postLikedCount;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
 
-    public PostResponse(Long id, UserNameResponse userNameResponse, String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public PostResponse(Long id, String userName, String title, String content, int postLikedCount, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
-        this.userNameResponse = userNameResponse;
+        this.userName = userName;
         this.title = title;
         this.content = content;
+        this.postLikedCount = postLikedCount;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }

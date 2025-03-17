49aba03f3f83dfbacb98f7600d40580205108ba4
JoeMinKyung <mk981015@naver.com>
2025-03-17 11:05:30 +0900

    feat: 댓글 좋아요

--- domain/comment/entity/Comment.java
+++ domain/comment/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.example.springbasicnewspeed.domain.comment.entity;
 
+import com.example.springbasicnewspeed.domain.auth.dto.AuthUser;
 import com.example.springbasicnewspeed.domain.post.entity.Post;
 import com.example.springbasicnewspeed.domain.user.entity.User;
 import jakarta.persistence.*;
@@ -10,6 +11,8 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Entity
@@ -41,9 +44,45 @@ public class Comment {
     @Column(nullable = false)
     private int commentLikedCount = 0;
 
+    @ManyToMany
+    @JoinTable(
+            name = "comment_likes",
+            joinColumns = @JoinColumn(name = "comment_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> commentLikedUsers = new HashSet<>();
+
     public Comment(String content, User user, Post post) {
         this.content = content;
         this.user = user;
         this.post = post;
+    }
+
+    public void update(String content) {
+        this.content = content;
+    }
+
+    public void addLike(User user) {
+        commentLikedUsers.add(user);
+    }
+
+    public void removeLike(User user) {
+        commentLikedUsers.remove(user);
+    }
+
+    public void updateCommentLikedCount(int count) {
+        this.commentLikedCount = count;
+    }
+
+    public int getCommentLikedCount() {
+        return commentLikedUsers.size();
+    }
+
+    public boolean isCommentOwner(User user) {
+        return this.user.equals(user);
+    }
+
+    public boolean isCommentOwnerByAuthUser(AuthUser authUser) {
+        return this.user.getId().equals(authUser.getId());
     }
 }

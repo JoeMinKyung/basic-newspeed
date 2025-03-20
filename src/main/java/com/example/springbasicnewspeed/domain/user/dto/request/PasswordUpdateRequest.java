95f650ed62c8622fd182f80fbb81e26c6820ca52
JoeMinKyung <mk981015@naver.com>
2025-03-20 13:24:20 +0900

    feat: @AllArgsConstructor 추가

--- src/main/java/com/example/springbasicnewspeed/domain/user/dto/request/PasswordUpdateRequest.java
+++ src/main/java/com/example/springbasicnewspeed/domain/user/dto/request/PasswordUpdateRequest.java
@@ -2,11 +2,13 @@ package com.example.springbasicnewspeed.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PasswordUpdateRequest {
 
     @NotBlank(message = "현재 비밀번호는 필수 항목입니다")

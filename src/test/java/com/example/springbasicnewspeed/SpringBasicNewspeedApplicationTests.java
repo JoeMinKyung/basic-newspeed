345af3b674fee87dd5ac64814fe78533e9d91f70
JoeMinKyung <mk981015@naver.com>
2025-03-21 00:31:07 +0900

    refactor

--- src/test/java/com/example/springbasicnewspeed/SpringBasicNewspeedApplicationTests.java
+++ src/test/java/com/example/springbasicnewspeed/SpringBasicNewspeedApplicationTests.java
@@ -1,10 +1,7 @@
 package com.example.springbasicnewspeed;
 
-import com.example.springbasicnewspeed.config.JpaAuditingConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
 class SpringBasicNewspeedApplicationTests {
@@ -12,5 +9,4 @@ class SpringBasicNewspeedApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }

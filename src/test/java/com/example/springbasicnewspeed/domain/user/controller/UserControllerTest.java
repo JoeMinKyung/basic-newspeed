5f1fb08e28dbf638165ee77cf8aec6d62a71ec7c
JoeMinKyung <mk981015@naver.com>
2025-03-20 21:11:42 +0900

    refactor

--- src/test/java/com/example/springbasicnewspeed/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/example/springbasicnewspeed/domain/user/controller/UserControllerTest.java
@@ -22,7 +22,6 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 

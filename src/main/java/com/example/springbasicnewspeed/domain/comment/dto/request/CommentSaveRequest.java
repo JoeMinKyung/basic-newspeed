08658ff8ad102efd8395026fd035e644146088ca
JoeMinKyung <mk981015@naver.com>
2025-03-20 23:10:37 +0900

    feat: @AllArgsConstructor 추가

--- src/main/java/com/example/springbasicnewspeed/domain/comment/dto/request/CommentSaveRequest.java
+++ src/main/java/com/example/springbasicnewspeed/domain/comment/dto/request/CommentSaveRequest.java
@@ -1,9 +1,11 @@
 package com.example.springbasicnewspeed.domain.comment.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CommentSaveRequest {
 
     @NotBlank
